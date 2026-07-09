1a84344d26c4d372c7aeeeb811fa6e02ce751702
Laurence F. Batolina <ridgip022026@ncmbnet.local>
2026-07-09 16:28:55 +0800

    feat: add NCMB logo SVG as favicon in browser tab

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,6 +6,9 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>@yield('title', 'CMMS - Repair & Maintenance')</title>
     
+    <!-- Favicon - NCMB Logo -->
+    <link rel="icon" type="image/svg+xml" href="{{ asset('images/ncmb-logo.svg') }}">
+    
     <!-- Fonts & Icons -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     
