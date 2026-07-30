a416f4d8b058250ee1356daff3622841786c13d4
Laurence F. Batolina <ridgip022026@ncmbnet.local>
2026-07-30 15:12:36 +0800

    fix: Revert inventory.js to asset() reference for working buttons

--- resources/views/inventory/index.blade.php
+++ resources/views/inventory/index.blade.php
@@ -1,4 +1,4 @@
-﻿@extends('layouts.app')
+@extends('layouts.app')
 
 @section('title', 'Inventory Masterlist | NCMB ICT System')
 @section('page-title', 'Inventory Masterlist')
@@ -702,7 +702,7 @@
 @endsection
 
 @section('scripts')
-@vite(['resources/js/inventory.js'])
+<script nonce="{{ \$cspNonce }}" src="{{ asset('js/inventory.js') }}?v={{ filemtime(public_path('js/inventory.js')) }}"></script>
 <script nonce="{{ $cspNonce }}">
     window.CMMS_INVENTORY_CAN_WRITE = @json($canWriteInventory);
     window.CMMS_IS_SUPPLY_ADMIN = @json(empty($isSuperAdminView) && $canWriteInventory); // Skip division/dept filtering for supply admin
